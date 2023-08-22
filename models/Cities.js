c6be8322a93fc2d83915dc676c65099fdc98b8b4
Alan Coiradas <coiradasalan@gmail.com>
2023-08-22 18:23:27 -0300

    Cambios en data
    Se hicieron modificaciones en la base de datos y en el modelo de los datos a almacenar. Se añadió una descripción de la ciudad, lugares turísticos para visitar, gastronomía local, y la moneda usada en dicha ciudad.

--- models/Cities.js
+++ models/Cities.js
@@ -5,7 +5,11 @@ let collection = 'cities';
 let citiesSchema = new Schema({
     cityName: {type: String, required: true},
     img: {type: String, required: true},
-    country: {type:String, required: true}
+    country: {type: String, required: true},
+    desc: {type: String},
+    touristSpots: {type: String},
+    gastronomy: {type: String},
+    currency: {type: String}
 },{
     timestamps: true
 })
