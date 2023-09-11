f2b75678759442c71a8a9f946b125e2272ecfb34
Alan Coiradas <coiradasalan@gmail.com>
2023-09-11 17:35:29 -0300

    middlewares
    Se añadieron los middlewares correspondientes a signin y signout al router de autenticación.

--- router/auth.router.js
+++ router/auth.router.js
@@ -1,9 +1,30 @@
 import express from 'express'
+import controller from '../controllers/auth.controller.js';
+import { accountExistsSignUp } from '../middlewares/auth/accountExistsSignUp.middleware.js';
+import { accountExistSignIn } from '../middlewares/auth/accountExistsSignIn.middleware.js';
+import { accountIsVerified } from '../middlewares/auth/accountIsVerified.middleware.js';
+import { passwordIsOk } from '../middlewares/auth/passwordIsOk.middleware.js';
+import { validator } from '../middlewares/validator.js';
+import { registerSchema, loginSchema } from '../schema/user.schema.js';
+
+const { signup, signin } = controller
 
 const router = express.Router()
 
-router.post('signin')
-router.post('signup')
+router.post('/signin', 
+    validator(loginSchema),
+    accountExistSignIn,
+    accountIsVerified, 
+    passwordIsOk,
+    signin
+)
+
+
+router.post('/signup',
+    validator(registerSchema),
+    accountExistsSignUp,
+    signup
+)
 
 export default router
 
