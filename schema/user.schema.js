6d08781ffc28e1d37c8a344a061f0f1f26feca01
Alan Coiradas <coiradasalan@gmail.com>
2023-09-11 00:48:40 -0300

    messages
    Se añadieron mensajes que se mostrarán en caso de que haya un error en la validación de alguno de los campos de usuario.

--- schema/user.schema.js
+++ schema/user.schema.js
@@ -5,22 +5,46 @@ export const createUserSchema = Joi.object({
         .required()
         .min(2)
         .max(30)
+        .messages({
+            'any.required': '',
+            'string.min': 'username should have at least 2 characters.',
+            'string.max': 'username should have less than 30 characters.',
+            'string.empty': 'user field is empty, please write the username you want to have.'
+        })
     ,
     email: Joi.string()
         .required()
         .email({
             minDomainSegments: 2
         })
-       /* .messages({
-            'any.required': 'The email is required'
-        }),*/,
+       .messages({
+            'any.required': 'The email is required.',
+            'string.empty': 'It seems you forgot to write your email, please write it and try again.',
+            'string.email': 'Please type a valid email. ex: mail@domail.com'
+        }),
     password: Joi.string()
         .required()
         .min(8)
         .max(35)
-        .alphanum(),
+        .alphanum()
+        .messages({
+            'any.required': 'Password is required.',
+            'string.empty': 'Please type a password for your account.',
+            'string.min': 'Password should have at least 8 characters.',
+            'string.max': 'Password should have less than 35 characters.',
+
+        }),
+    country: Joi.string()
+        .required
+        .messages({
+            'any.required': 'Country is required.'
+        }),
     
     image: Joi.string()
         .required()
         .uri()
+        .messages({
+            'any.required': 'Image is required.',
+            'string.uri': 'Image should be a valid URL.'
+        })
 })
